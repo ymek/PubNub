ab47390f5cf209aad9f8dbbbd1b4482467602e4c
Valentin Tuller <tullerval@gmail.com>
2014-01-09 15:50:29 +0200

    add empty test

---
+++ iOS/iPadDemoApp/Tests/Tests.m
@@ -0,0 +1,34 @@
+//
+//  Tests.m
+//  Tests
+//
+//  Created by Valentin Tuller on 1/9/14.
+//  Copyright (c) 2014 PubNub Inc. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Tests : SenTestCase
+
+@end
+
+@implementation Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+//    STFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
