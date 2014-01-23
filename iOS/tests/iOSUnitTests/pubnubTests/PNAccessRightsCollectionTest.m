ae9b587134daa1d6a8414c6150df11feeb2fedf9
Valentin Tuller <tullerval@gmail.com>
2014-01-23 17:59:05 +0200

    finished PNAccessRightsCollection test

--- iOS/tests/iOSUnitTests/pubnubTests/PNAccessRightsCollectionTest.m
+++ iOS/tests/iOSUnitTests/pubnubTests/PNAccessRightsCollectionTest.m
@@ -11,8 +11,10 @@
 #import "PNAccessRightsCollection+Protected.h"
 #import "PNAccessRightsInformation+Protected.h"
 #import "PNAccessRightsInformation.h"
-#import "PNChannel.h"
+#import "PNAccessRightOptions+Protected.h"
+#import "PNAccessRightOptions.h"
 #import "PNStructures.h"
+#import "PNChannel.h"
 
 @interface PNAccessRightsCollection (test)
 
@@ -109,6 +111,107 @@
 	STAssertTrue( info.channel == channel, @"");
 	STAssertTrue( [info.authorizationKey isEqualToString: @"client"], @"");
 	STAssertTrue( info.accessPeriodDuration == 123, @"");
+}
+
+-(void)testAccessRightsInformationForAllClientAuthorizationKeys {
+	PNAccessRightsCollection *collection = [PNAccessRightsCollection accessRightsCollectionForApplication: @"key" andAccessRightsLevel: PNApplicationAccessRightsLevel];
+	PNChannel *channel = [PNChannel channelWithName: @"channel"];
+	PNAccessRightsInformation *info = [PNAccessRightsInformation accessRightsInformationForLevel: PNChannelAccessRightsLevel rights: PNReadAccessRight | PNWriteAccessRight applicationKey: @"key" forChannel: channel client: @"client" accessPeriod: 123];
+	[collection storeClientAccessRightsInformation: info forChannel: channel];
+
+	NSArray *arr = [collection accessRightsInformationForAllClientAuthorizationKeys];
+	STAssertTrue( [arr count] == 1, @"");
+	STAssertTrue( arr[0] == info, @"");
+}
+
+-(void)testAccessRightsInformationForClientAuthorizationKey {
+	PNAccessRightsCollection *collection = [PNAccessRightsCollection accessRightsCollectionForApplication: @"key" andAccessRightsLevel: PNApplicationAccessRightsLevel];
+	PNChannel *channel = [PNChannel channelWithName: @"channel"];
+	PNAccessRightsInformation *info = [PNAccessRightsInformation accessRightsInformationForLevel: PNChannelAccessRightsLevel rights: PNReadAccessRight | PNWriteAccessRight applicationKey: @"key" forChannel: channel client: @"client" accessPeriod: 123];
+	[collection storeChannelAccessRightsInformation: info];
+
+	NSArray *arr = [collection accessRightsInformationForClientAuthorizationKey: @"client"];
+	STAssertTrue( [arr count] == 1, @"");
+	STAssertTrue( arr[0] == info, @"");
+
+	arr = [collection accessRightsInformationForClientAuthorizationKey: @"invalid_auth_key"];
+	STAssertTrue( [arr count] == 0, @"");
+}
+
+-(void)testAccessRightsInformationClientAuthorizationKey {
+	PNAccessRightsCollection *collection = [PNAccessRightsCollection accessRightsCollectionForApplication: @"key" andAccessRightsLevel: PNApplicationAccessRightsLevel];
+	PNChannel *channel = [PNChannel channelWithName: @"channel"];
+//	PNAccessRightsInformation *info = [PNAccessRightsInformation accessRightsInformationForLevel: PNChannelAccessRightsLevel rights: PNReadAccessRight | PNWriteAccessRight applicationKey: @"key" forChannel: channel client: @"client" accessPeriod: 123];
+
+	PNAccessRightsInformation *info = [collection accessRightsInformationClientAuthorizationKey: @"key" onChannel: channel];
+	STAssertTrue( info.level == PNUserAccessRightsLevel, @"");
+	STAssertTrue( info.rights == PNUnknownAccessRights, @"");
+	STAssertTrue( [info.subscriptionKey isEqualToString: @"key"] == TRUE, @"");
+	STAssertTrue( info.channel == channel, @"");
+	STAssertTrue( [info.authorizationKey isEqualToString: @"key"] == TRUE, @"");
+	STAssertTrue( info.accessPeriodDuration == 0, @"");
+
+	info = [PNAccessRightsInformation accessRightsInformationForLevel: PNChannelAccessRightsLevel rights: PNReadAccessRight | PNWriteAccessRight applicationKey: @"key" forChannel: channel client: @"client" accessPeriod: 123];
+	[collection storeChannelAccessRightsInformation: info];
+	info = [collection accessRightsInformationClientAuthorizationKey: @"key" onChannel: channel];
+	STAssertTrue( info.level == PNUserAccessRightsLevel, @"");
+	STAssertTrue( info.rights == (PNReadAccessRight | PNWriteAccessRight), @"");
+	STAssertTrue( [info.subscriptionKey isEqualToString: @"key"] == TRUE, @"");
+	STAssertTrue( info.channel == channel, @"");
+	STAssertTrue( [info.authorizationKey isEqualToString: @"key"], @"");
+}
+
+-(void)testStoreApplicationAccessRightsInformation {
+	PNAccessRightsCollection *collection = [PNAccessRightsCollection accessRightsCollectionForApplication: @"key" andAccessRightsLevel: PNApplicationAccessRightsLevel];
+	PNAccessRightsInformation *info = [PNAccessRightsInformation accessRightsInformationForLevel: PNChannelAccessRightsLevel rights: PNReadAccessRight | PNWriteAccessRight applicationKey: @"key" forChannel: nil client: @"client" accessPeriod: 123];
+	[collection storeApplicationAccessRightsInformation: info];
+	STAssertTrue( collection.applicationAccessRightsInformation == info, @"");
+}
+
+-(void)testStoreClientAccessRightsInformation {
+	PNAccessRightsCollection *collection = [PNAccessRightsCollection accessRightsCollectionForApplication: @"key" andAccessRightsLevel: PNApplicationAccessRightsLevel];
+	PNChannel *channel = [PNChannel channelWithName: @"channel"];
+	PNAccessRightsInformation *info = [PNAccessRightsInformation accessRightsInformationForLevel: PNUserAccessRightsLevel rights: PNReadAccessRight | PNWriteAccessRight applicationKey: @"key" forChannel: channel client: @"client" accessPeriod: 123];
+
+	[collection storeClientAccessRightsInformation: info forChannel: channel];
+	NSArray *arr = [collection accessRightsForClientsOnChannel: channel];
+	STAssertTrue( [arr count] == 1, @"");
+	info = arr[0];
+	STAssertTrue( info.level == PNUserAccessRightsLevel, @"");
+	STAssertTrue( info.rights == (PNReadAccessRight | PNWriteAccessRight), @"");
+	STAssertTrue( [info.subscriptionKey isEqualToString: @"key"] == TRUE, @"");
+	STAssertTrue( info.channel == channel, @"");
+	STAssertTrue( [info.authorizationKey isEqualToString: @"client"], @"");
+	STAssertTrue( info.accessPeriodDuration == 123, @"");
+}
+
+-(void)testCorrelateAccessRightsWithOptions {
+	PNAccessRightsCollection *collection = [PNAccessRightsCollection accessRightsCollectionForApplication: @"key" andAccessRightsLevel: PNApplicationAccessRightsLevel];
+
+	PNAccessRightOptions *options = [[PNAccessRightOptions alloc] init];
+	options.level = PNApplicationAccessRightsLevel;
+	[collection correlateAccessRightsWithOptions: options];
+	STAssertTrue( collection.level == options.level, @"");
+}
+
+-(void)testPopulateAccessRightsFrom {
+	PNAccessRightsCollection *collection = [PNAccessRightsCollection accessRightsCollectionForApplication: @"key" andAccessRightsLevel: PNApplicationAccessRightsLevel];
+	PNChannel *channel = [PNChannel channelWithName: @"channel"];
+
+	PNAccessRightsInformation *info1 = [PNAccessRightsInformation accessRightsInformationForLevel: PNUserAccessRightsLevel rights: PNReadAccessRight | PNWriteAccessRight applicationKey: @"key" forChannel: channel client: @"client" accessPeriod: 123];
+	PNAccessRightsInformation *info2 = [[PNAccessRightsInformation alloc] init];
+	[collection populateAccessRightsFrom: info1 to: info2];
+	STAssertTrue( info2.rights == (PNReadAccessRight | PNWriteAccessRight), @"");
+
+	info1 = [PNAccessRightsInformation accessRightsInformationForLevel: PNUserAccessRightsLevel rights: PNReadAccessRight applicationKey: @"key" forChannel: channel client: @"client" accessPeriod: 123];
+	info2 = [[PNAccessRightsInformation alloc] init];
+	[collection populateAccessRightsFrom: info1 to: info2];
+	STAssertTrue( info2.rights == (PNReadAccessRight), @"");
+
+	info1 = [PNAccessRightsInformation accessRightsInformationForLevel: PNUserAccessRightsLevel rights: PNWriteAccessRight applicationKey: @"key" forChannel: channel client: @"client" accessPeriod: 123];
+	info2 = [[PNAccessRightsInformation alloc] init];
+	[collection populateAccessRightsFrom: info1 to: info2];
+	STAssertTrue( info2.rights == (PNWriteAccessRight), @"");
 }
 
 @end
