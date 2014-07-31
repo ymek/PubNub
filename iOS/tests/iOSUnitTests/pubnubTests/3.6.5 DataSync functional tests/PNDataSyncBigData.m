878af88694ca185d6f883cdcde9ef709be26c277
Vadim Osovets <vosovets@micro-b.net>
2014-07-31 15:35:02 +0300

    [#75801212] Add more tests for big amount of data.

--- iOS/tests/iOSUnitTests/pubnubTests/3.6.5 DataSync functional tests/PNDataSyncBigData.m
+++ iOS/tests/iOSUnitTests/pubnubTests/3.6.5 DataSync functional tests/PNDataSyncBigData.m
@@ -11,7 +11,7 @@
 static NSString * const kTestObject = @"ios_test_db_big_1";
 static NSString * const kTestPathFirst = @"test";
 static NSString * const kTestPathComplex = @"test.second";
-static const NSUInteger kTestStandardTimeout = 20;
+static const NSUInteger kTestStandardTimeout = 30;
 
 @interface PNDataSyncBigData : SenTestCase
 
@@ -50,7 +50,7 @@ PNDelegate
     [super tearDown];
 }
 
-- (void)t1estBigData1Kb
+- (void)testBigData1Kb
 {
     _testReplace1Kb = dispatch_group_create();
     
@@ -90,14 +90,14 @@ PNDelegate
     _testReplace1Kb = NULL;
 }
 
-- (void)testBigData1Mb
+- (void)t1estBigData1Mb
 {
     _testReplace1Mb = dispatch_group_create();
     
     dispatch_group_enter(_testReplace1Mb);
     
 //    NSDictionary *testDict = [self generateTestDictWithDepth:6 maxKeysOnLevel:5];
-    NSDictionary *testDict = [self generateTestDictWithDepth:4 maxKeysOnLevel:5];
+    NSDictionary *testDict = [self generateTestDictWithDepth:5 maxKeysOnLevel:5];
 
     
     NSData *data = [NSPropertyListSerialization dataWithPropertyList:[NSDictionary dictionaryWithDictionary:testDict]
@@ -114,7 +114,7 @@ PNDelegate
     }];
     
     STAssertFalse([GCDWrapper isGroup:_testReplace1Mb
-                    timeoutFiredValue:kTestStandardTimeout], @"Simple replace Object - failed.");
+                    timeoutFiredValue:kTestStandardTimeout], @"Replace object with data size: %luKb failed", (unsigned long)[data length]/1024);
     
     dispatch_group_enter(_testReplace1Mb);
     [PubNub fetchObject:kTestObject withCompletionHandlingBlock:^(PNObject *obj, PNError *error) {
@@ -126,9 +126,137 @@ PNDelegate
     }];
     
     STAssertFalse([GCDWrapper isGroup:_testReplace1Mb
-                    timeoutFiredValue:kTestStandardTimeout], @"Simple fetch Object - failed.");
+                    timeoutFiredValue:kTestStandardTimeout], @"Fetch object with data size: %luKb failed", (unsigned long)[data length]/1024);
     
     _testReplace1Mb = NULL;
+}
+
+- (void)testBigData100Mb
+{
+    _testReplace100Mb = dispatch_group_create();
+
+    // 1211Mb - 10/5
+    // 44Mb - 8/5
+    // 1811Mb - 8/8
+    // 185Mb - 8/6
+    // 1169Mb - 9/6
+    // 573Mb - 5/23
+    // 94Mb - 5/16
+    
+    NSDictionary *testDict = [self generateTestDictWithDepth:5 maxKeysOnLevel:16];
+    
+    NSData *data = [NSPropertyListSerialization dataWithPropertyList:[NSDictionary dictionaryWithDictionary:testDict]
+                                                              format:NSPropertyListXMLFormat_v1_0 options:NSProprietaryStringEncoding error:NULL];
+    NSLog(@"Data size: %luMb", (unsigned long)[data length]/(1024 * 1024));
+    
+    [PubNub replaceObject:kTestObject withData:testDict andCompletionHandlingBlock:^(PNObjectModificationInformation *objectModification, PNError *error) {
+        
+        if (error) {
+            STFail(@"Cannot set object with size:  %luMb", (unsigned long)[data length]/(1024 * 1024));
+        }
+        
+        dispatch_group_leave(_testReplace100Mb);
+    }];
+    
+    STAssertFalse([GCDWrapper isGroup:_testReplace100Mb
+                    timeoutFiredValue:kTestStandardTimeout], @"Replace object with data size: %luMb failed", (unsigned long)[data length]/(1024 * 1024));
+    
+    dispatch_group_enter(_testReplace100Mb);
+    [PubNub fetchObject:kTestObject withCompletionHandlingBlock:^(PNObject *obj, PNError *error) {
+        if (error) {
+            STFail(@"Cannot set object with size:  %luMb", (unsigned long)[data length]/(1024 * 1024));
+        }
+        
+        dispatch_group_leave(_testReplace100Mb);
+    }];
+    
+    STAssertFalse([GCDWrapper isGroup:_testReplace100Mb
+                    timeoutFiredValue:kTestStandardTimeout], @"Fetch object with data size: %luMb failed", (unsigned long)[data length]/(1024 * 1024));
+    
+    _testReplace100Mb = NULL;
+}
+
+- (void)testBigData500Mb
+{
+    _testReplace500Mb = dispatch_group_create();
+    
+    dispatch_group_enter(_testReplace500Mb);
+    // 1211Mb - 10/5
+    // 44Mb - 8/5
+    // 1811Mb - 8/8
+    // 185Mb - 8/6
+    // 1169Mb - 9/6
+    
+    NSDictionary *testDict = [self generateTestDictWithDepth:5 maxKeysOnLevel:23];
+    
+    NSData *data = [NSPropertyListSerialization dataWithPropertyList:[NSDictionary dictionaryWithDictionary:testDict]
+                                                              format:NSPropertyListXMLFormat_v1_0 options:NSProprietaryStringEncoding error:NULL];
+    NSLog(@"Data size: %luMb", (unsigned long)[data length]/(1024 * 1024));
+    
+    [PubNub replaceObject:kTestObject withData:testDict andCompletionHandlingBlock:^(PNObjectModificationInformation *objectModification, PNError *error) {
+        
+        if (error) {
+            STFail(@"Cannot set object with size:  %luMb", (unsigned long)[data length]/(1024 * 1024));
+        }
+        
+        dispatch_group_leave(_testReplace500Mb);
+    }];
+    
+    STAssertFalse([GCDWrapper isGroup:_testReplace500Mb
+                    timeoutFiredValue:kTestStandardTimeout], @"Replace object with data size: %luMb failed", (unsigned long)[data length]/(1024 * 1024));
+    
+    dispatch_group_enter(_testReplace500Mb);
+    [PubNub fetchObject:kTestObject withCompletionHandlingBlock:^(PNObject *obj, PNError *error) {
+        if (error) {
+            STFail(@"Cannot set object with size:  %luMb", (unsigned long)[data length]/(1024 * 1024));
+        }
+        
+        dispatch_group_leave(_testReplace500Mb);
+    }];
+    
+    STAssertFalse([GCDWrapper isGroup:_testReplace500Mb
+                    timeoutFiredValue:kTestStandardTimeout], @"Fetch object with data size: %luMb failed", (unsigned long)[data length]/(1024 * 1024));
+    
+    _testReplace500Mb = NULL;
+}
+
+- (void)testBigData1Gb
+{
+    _testReplace1Gb = dispatch_group_create();
+    
+    dispatch_group_enter(_testReplace1Gb);
+    
+    NSDictionary *testDict = [self generateTestDictWithDepth:9 maxKeysOnLevel:6];
+    
+    NSData *data = [NSPropertyListSerialization dataWithPropertyList:[NSDictionary dictionaryWithDictionary:testDict]
+                                                              format:NSPropertyListXMLFormat_v1_0 options:NSProprietaryStringEncoding error:NULL];
+    NSLog(@"Data size: %luMb", (unsigned long)[data length]/(1024 * 1024));
+    
+    [PubNub replaceObject:kTestObject withData:testDict andCompletionHandlingBlock:^(PNObjectModificationInformation *objectModification, PNError *error) {
+        
+        if (error) {
+            STFail(@"Cannot set object with size:  %luMb", (unsigned long)[data length]/(1024 * 1024));
+        }
+        
+        dispatch_group_leave(_testReplace1Gb);
+    }];
+    
+    STAssertFalse([GCDWrapper isGroup:_testReplace1Gb
+                    timeoutFiredValue:kTestStandardTimeout], @"Replace object with data size: %luMb failed", (unsigned long)[data length]/(1024 * 1024));
+    
+    dispatch_group_enter(_testReplace1Gb);
+    [PubNub fetchObject:kTestObject withCompletionHandlingBlock:^(PNObject *obj, PNError *error) {
+        if (error) {
+            STFail(@"Cannot set object with size:  %luMb", (unsigned long)[data length]/(1024 * 1024));
+        }
+        
+        dispatch_group_leave(_testReplace1Gb);
+    }];
+    
+    STAssertFalse([GCDWrapper isGroup:_testReplace1Gb
+                    timeoutFiredValue:kTestStandardTimeout], @"Fetch object with data size: %luMb failed", (unsigned long)[data length]/(1024 * 1024));
+    
+    _testReplace1Gb = NULL;
 }
 
 #pragma mark - Generate test data
