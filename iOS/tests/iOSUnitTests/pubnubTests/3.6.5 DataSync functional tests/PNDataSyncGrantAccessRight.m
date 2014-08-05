55103dc70b9fc52a4143d352f7f27a386e2f60c5
Vadim Osovets <vosovets@micro-b.net>
2014-08-05 19:28:10 +0300

    [#76137642] Update tests for PAM feature.

--- iOS/tests/iOSUnitTests/pubnubTests/3.6.5 DataSync functional tests/PNDataSyncGrantAccessRight.m
+++ iOS/tests/iOSUnitTests/pubnubTests/3.6.5 DataSync functional tests/PNDataSyncGrantAccessRight.m
@@ -20,8 +20,12 @@ PNDelegate
 @end
 
 @implementation PNDataSyncGrantAccessRight {
-        dispatch_group_t _testPAM;
+        dispatch_group_t _testPAM1;
         dispatch_group_t _testPAM2;
+        dispatch_group_t _testPAM3;
+        dispatch_group_t _testPAM4;
+        dispatch_group_t _testPAM5;
+        dispatch_group_t _testPAM6;
 }
 
 - (void)setUp
@@ -47,12 +51,14 @@ PNDelegate
 
 #pragma mark - Tests
 
+#pragma mark - Read
+
 - (void)testAccessRightsReadForObject {
-    _testPAM = dispatch_group_create();
+    _testPAM1 = dispatch_group_create();
     
-    dispatch_group_enter(_testPAM);
-    dispatch_group_enter(_testPAM);
-    dispatch_group_enter(_testPAM);
+    dispatch_group_enter(_testPAM1);
+    dispatch_group_enter(_testPAM1);
+    dispatch_group_enter(_testPAM1);
     
     [[PNObservationCenter defaultCenter] addObjectAccessRightsChangeObserver:self withBlock:^(PNObjectAccessRightsCollection *collection, PNError *error) {
         
@@ -71,7 +77,7 @@ PNDelegate
             STFail(@"Object right change did fail with error: %@", [error localizedDescription]);
         }
         
-        dispatch_group_leave(_testPAM);
+        dispatch_group_leave(_testPAM1);
     }];
     
     [[NSNotificationCenter defaultCenter] addObserver:self
@@ -82,11 +88,254 @@ PNDelegate
     [PubNub grantReadAccessRightForObject:kTestObject
                                 forPeriod:10];
     
+    STAssertFalse([GCDWrapper isGroup:_testPAM1
+                    timeoutFiredValue:kTestStandardTimeout], @"Timeout Received.");
+    
     [[PNObservationCenter defaultCenter] removeObjectAccessRightsObserver:self];
     [[NSNotificationCenter defaultCenter] removeObserver:self];
     
-    _testPAM = NULL;
+    _testPAM1 = NULL;
 }
+
+- (void)testReadAccessRightForObjectsWithCompletionHandlingBlock {
+    _testPAM3 = dispatch_group_create();
+    
+    dispatch_group_enter(_testPAM3);
+    dispatch_group_enter(_testPAM3);
+    dispatch_group_enter(_testPAM3);
+    dispatch_group_enter(_testPAM3);
+    
+    [[PNObservationCenter defaultCenter] addObjectAccessRightsChangeObserver:self withBlock:^(PNObjectAccessRightsCollection *collection, PNError *error) {
+        
+        if (error == nil) {
+            
+            // PubNub client successfully changed access rights for cloud 'object' level.
+        }
+        else {
+            
+            // PubNub client did fail to changed access rights for cloud 'object' level.
+            //
+            // Always check 'error.code' to find out what caused error (check PNErrorCodes header file and use -localizedDescription /
+            // -localizedFailureReason and -localizedRecoverySuggestion to get human readable description for error).
+            // 'error.associatedObject' contains PNObjectAccessRightOptions instance which describes access level for which change
+            // has been requested.
+            STFail(@"Object right change did fail with error: %@", [error localizedDescription]);
+        }
+        
+        dispatch_group_leave(_testPAM3);
+    }];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(receivedNotification:) name:kPNClientObjectAccessRightsChangeDidCompleteNotification
+                                               object:nil];
+    
+    [PubNub grantReadAccessRightForObjects:@[kTestObject, @"ios_test_db_access1", @"ios_test_db_access2"] forPeriod:10 withCompletionHandlingBlock:^(PNObjectAccessRightsCollection *collection, PNError *error) {
+        if (error == nil) {
+            
+            // PubNub client successfully changed access rights for cloud 'object' level.
+        }
+        else {
+            
+            // PubNub client did fail to changed access rights for cloud 'object' level.
+            //
+            // Always check 'error.code' to find out what caused error (check PNErrorCodes header file and use -localizedDescription /
+            // -localizedFailureReason and -localizedRecoverySuggestion to get human readable description for error).
+            // 'error.associatedObject' contains PNObjectAccessRightOptions instance which describes access level for which change
+            // has been requested.
+            STFail(@"Object right change did fail with error: %@", [error localizedDescription]);
+        }
+        
+        dispatch_group_leave(_testPAM3);
+
+    }];
+    
+//    [PubNub grantReadAccessRightForObject:<#(NSString *)#> forPeriod:<#(NSInteger)#> clients:<#(NSArray *)#> withCompletionHandlingBlock:<#^(PNObjectAccessRightsCollection *, PNError *)handlerBlock#>]
+    
+//    [PubNub grantReadAccessRightForObject:<#(NSString *)#> forPeriod:<#(NSInteger)#> clients:<#(NSArray *)#>]
+    
+//    [PubNub grantReadAccessRightForObject:<#(NSString *)#> forPeriod:<#(NSInteger)#> client:<#(NSString *)#> withCompletionHandlingBlock:<#^(PNObjectAccessRightsCollection *, PNError *)handlerBlock#>]
+    
+//    [PubNub grantReadAccessRightForObject:<#(NSString *)#> forPeriod:<#(NSInteger)#> client:<#(NSString *)#>]
+    
+    STAssertFalse([GCDWrapper isGroup:_testPAM3
+                    timeoutFiredValue:kTestStandardTimeout], @"Timeout Received.");
+    
+    [[PNObservationCenter defaultCenter] removeObjectAccessRightsObserver:self];
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+    
+    _testPAM3 = NULL;
+}
+
+- (void)testReadAccessRightForObjects {
+    _testPAM4 = dispatch_group_create();
+    
+    dispatch_group_enter(_testPAM4);
+    dispatch_group_enter(_testPAM4);
+    dispatch_group_enter(_testPAM4);
+    
+    [[PNObservationCenter defaultCenter] addObjectAccessRightsChangeObserver:self withBlock:^(PNObjectAccessRightsCollection *collection, PNError *error) {
+        
+        if (error == nil) {
+            
+            // PubNub client successfully changed access rights for cloud 'object' level.
+        }
+        else {
+            
+            // PubNub client did fail to changed access rights for cloud 'object' level.
+            //
+            // Always check 'error.code' to find out what caused error (check PNErrorCodes header file and use -localizedDescription /
+            // -localizedFailureReason and -localizedRecoverySuggestion to get human readable description for error).
+            // 'error.associatedObject' contains PNObjectAccessRightOptions instance which describes access level for which change
+            // has been requested.
+            STFail(@"Object right change did fail with error: %@", [error localizedDescription]);
+        }
+        
+        dispatch_group_leave(_testPAM4);
+    }];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(receivedNotification:) name:kPNClientObjectAccessRightsChangeDidCompleteNotification
+                                               object:nil];
+    
+    [PubNub grantReadAccessRightForObjects:@[kTestObject, @"ios_test_db_access1", @"ios_test_db_access2"] forPeriod:10];
+    
+    STAssertFalse([GCDWrapper isGroup:_testPAM4
+                    timeoutFiredValue:kTestStandardTimeout], @"Timeout Received.");
+    
+    [[PNObservationCenter defaultCenter] removeObjectAccessRightsObserver:self];
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+    
+    _testPAM4 = NULL;
+}
+
+- (void)testReadAccessRightForObjectsAndClientsWithCompletionBlock {
+    _testPAM5 = dispatch_group_create();
+    
+    dispatch_group_enter(_testPAM5);
+    dispatch_group_enter(_testPAM5);
+    dispatch_group_enter(_testPAM5);
+    dispatch_group_enter(_testPAM5);
+    
+    [[PNObservationCenter defaultCenter] addObjectAccessRightsChangeObserver:self withBlock:^(PNObjectAccessRightsCollection *collection, PNError *error) {
+        
+        if (error == nil) {
+            
+            // PubNub client successfully changed access rights for cloud 'object' level.
+        }
+        else {
+            
+            // PubNub client did fail to changed access rights for cloud 'object' level.
+            //
+            // Always check 'error.code' to find out what caused error (check PNErrorCodes header file and use -localizedDescription /
+            // -localizedFailureReason and -localizedRecoverySuggestion to get human readable description for error).
+            // 'error.associatedObject' contains PNObjectAccessRightOptions instance which describes access level for which change
+            // has been requested.
+            STFail(@"Object right change did fail with error: %@", [error localizedDescription]);
+        }
+        
+        dispatch_group_leave(_testPAM5);
+    }];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(receivedNotification:) name:kPNClientObjectAccessRightsChangeDidCompleteNotification
+                                               object:nil];
+    
+    [PubNub grantReadAccessRightForObject:kTestObject
+                                forPeriod:10
+                                  clients:@[@"client1", @"client2"]
+              withCompletionHandlingBlock:^(PNObjectAccessRightsCollection *collection, PNError *error) {
+                  if (error == nil) {
+                      
+                      // PubNub client successfully changed access rights for cloud 'object' level.
+                  }
+                  else {
+                      
+                      // PubNub client did fail to changed access rights for cloud 'object' level.
+                      //
+                      // Always check 'error.code' to find out what caused error (check PNErrorCodes header file and use -localizedDescription /
+                      // -localizedFailureReason and -localizedRecoverySuggestion to get human readable description for error).
+                      // 'error.associatedObject' contains PNObjectAccessRightOptions instance which describes access level for which change
+                      // has been requested.
+                      STFail(@"Object right change did fail with error: %@", [error localizedDescription]);
+                  }
+                  
+                  dispatch_group_leave(_testPAM5);
+              }];
+    
+    STAssertFalse([GCDWrapper isGroup:_testPAM5
+                    timeoutFiredValue:kTestStandardTimeout], @"Timeout Received.");
+    
+    [[PNObservationCenter defaultCenter] removeObjectAccessRightsObserver:self];
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+    
+    _testPAM5 = NULL;
+}
+
+- (void)testGrantReadAccessRightForObjectWithCompletionHandlerBlock {
+    _testPAM6 = dispatch_group_create();
+    
+    dispatch_group_enter(_testPAM6);
+    dispatch_group_enter(_testPAM6);
+    dispatch_group_enter(_testPAM6);
+    dispatch_group_enter(_testPAM6);
+    
+    [[PNObservationCenter defaultCenter] addObjectAccessRightsChangeObserver:self withBlock:^(PNObjectAccessRightsCollection *collection, PNError *error) {
+        
+        if (error == nil) {
+            
+            // PubNub client successfully changed access rights for cloud 'object' level.
+        }
+        else {
+            
+            // PubNub client did fail to changed access rights for cloud 'object' level.
+            //
+            // Always check 'error.code' to find out what caused error (check PNErrorCodes header file and use -localizedDescription /
+            // -localizedFailureReason and -localizedRecoverySuggestion to get human readable description for error).
+            // 'error.associatedObject' contains PNObjectAccessRightOptions instance which describes access level for which change
+            // has been requested.
+            STFail(@"Object right change did fail with error: %@", [error localizedDescription]);
+        }
+        
+        dispatch_group_leave(_testPAM6);
+    }];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(receivedNotification:) name:kPNClientObjectAccessRightsChangeDidCompleteNotification
+                                               object:nil];
+    
+    [PubNub grantReadAccessRightForObjects:@[kTestObject, @"ios_test_db_access1", @"ios_test_db_access2"] forPeriod:10 withCompletionHandlingBlock:^(PNObjectAccessRightsCollection *collection, PNError *error) {
+        if (error == nil) {
+            
+            // PubNub client successfully changed access rights for cloud 'object' level.
+        }
+        else {
+            
+            // PubNub client did fail to changed access rights for cloud 'object' level.
+            //
+            // Always check 'error.code' to find out what caused error (check PNErrorCodes header file and use -localizedDescription /
+            // -localizedFailureReason and -localizedRecoverySuggestion to get human readable description for error).
+            // 'error.associatedObject' contains PNObjectAccessRightOptions instance which describes access level for which change
+            // has been requested.
+            STFail(@"Object right change did fail with error: %@", [error localizedDescription]);
+        }
+        
+        dispatch_group_leave(_testPAM6);
+    }];
+    
+    //    [PubNub grantReadAccessRightForObject:<#(NSString *)#> forPeriod:<#(NSInteger)#> clients:<#(NSArray *)#>]
+    //    [PubNub grantReadAccessRightForObject:<#(NSString *)#> forPeriod:<#(NSInteger)#> client:<#(NSString *)#> withCompletionHandlingBlock:<#^(PNObjectAccessRightsCollection *, PNError *)handlerBlock#>]
+    //    [PubNub grantReadAccessRightForObject:<#(NSString *)#> forPeriod:<#(NSInteger)#> client:<#(NSString *)#>]
+    
+    STAssertFalse([GCDWrapper isGroup:_testPAM6
+                    timeoutFiredValue:kTestStandardTimeout], @"Timeout Received.");
+    
+    [[PNObservationCenter defaultCenter] removeObjectAccessRightsObserver:self];
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+    
+    _testPAM6 = NULL;
+}
+
+#pragma mark - Write
 
 - (void)testAccessRightsWriteForObject {
     _testPAM2 = dispatch_group_create();
@@ -134,8 +383,19 @@ PNDelegate
 - (void)pubnubClient:(PubNub *)client didChangeObjectAccessRights:(PNObjectAccessRightsCollection *)accessRightsCollection {
     
     // PubNub client successfully changed access rights for cloud 'object' level.
-    
-    dispatch_group_leave(_testPAM);
+    if (_testPAM1) {
+        dispatch_group_leave(_testPAM1);
+    } else if (_testPAM2){
+        dispatch_group_leave(_testPAM2);
+    } else if (_testPAM3) {
+        dispatch_group_leave(_testPAM3);
+    } else if (_testPAM4) {
+        dispatch_group_leave(_testPAM4);
+    } else if (_testPAM5) {
+        dispatch_group_leave(_testPAM5);
+    } else if (_testPAM6) {
+        dispatch_group_leave(_testPAM6);
+    }
 }
 
 - (void)pubnubClient:(PubNub *)client objectAccessRightsChangeDidFailWithError:(PNError *)error {
@@ -149,14 +409,36 @@ PNDelegate
     
     STFail(@"Object right change did fail with error: %@", [error localizedDescription]);
     
-    dispatch_group_leave(_testPAM);
+    if (_testPAM1) {
+        dispatch_group_leave(_testPAM1);
+    } else if (_testPAM2){
+        dispatch_group_leave(_testPAM2);
+    } else if (_testPAM3) {
+        dispatch_group_leave(_testPAM3);
+    } else if (_testPAM4) {
+        dispatch_group_leave(_testPAM4);
+    } else if (_testPAM5) {
+        dispatch_group_leave(_testPAM5);
+    } else if (_testPAM6) {
+        dispatch_group_leave(_testPAM6);
+    }
 }
 
 #pragma mark - Notifications
 
 - (void)receivedNotification:(NSNotification *)notif {
-    if (_testPAM != NULL) {
-        dispatch_group_leave(_testPAM);
+    if (_testPAM1) {
+        dispatch_group_leave(_testPAM1);
+    } else if (_testPAM2){
+        dispatch_group_leave(_testPAM2);
+    } else if (_testPAM3) {
+        dispatch_group_leave(_testPAM3);
+    } else if (_testPAM4) {
+        dispatch_group_leave(_testPAM4);
+    } else if (_testPAM5) {
+        dispatch_group_leave(_testPAM5);
+    } else if (_testPAM6) {
+        dispatch_group_leave(_testPAM6);
     }
 }
 
